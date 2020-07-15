6747901e4e7e847fc0550038b02d35d7c172e5fa
Jacob Viti <thevitij64@gmail.com>
2020-07-15 04:10:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,5 @@
 # Add  code here!
 def prime?(number)
-
-if number < 2
-  return false
-elsif number = 2
-  return true
-end
-
 checker = 2
  while checker < number
   return false if checker / number == 0
